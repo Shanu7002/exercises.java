432b94ca56203ad3a8cc44c508830ba305446ee1
edumu <eduardoaum2007@gmail.com>
2025-09-28 23:22:16 -0300

    exercise 11 and 12 complete

---
+++ FirstStep/11-20/exercise11/src/Main.java
@@ -0,0 +1,15 @@
+//11. Check if a number is positive, negative, or zero.
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        if(n > 0){
+            System.out.print("The number is positive");
+        } else if (n < 0){
+            System.out.print("The number is negative");
+        } else {
+            System.out.print("The number is zero");
+        }
+
+    }
+}
---
+++ FirstStep/11-20/exercise13/src/Main.java
@@ -0,0 +1,10 @@
+//12. Ask for a number and say if it is a multiple of 5.
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        if(n % 5 == 0){
+            System.out.print("The number is multiple of 5");
+        }
+    }
+}
